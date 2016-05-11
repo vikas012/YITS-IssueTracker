ebea1ca32c60b9f591b04fcdd7cd91204b5e02a7
prajvi.jain <prajvi.jain@YITRNG30DT.Yash.com>
2016-05-11 13:08:32 +0530

    Issue updated. IssueDaoImpl, IssueServiceImpl added.

---
+++ yashissuetrackersystem/yashissuetrackersystem-core/src/main/java/com/yash/yits/dao/hibernate/IssueDaoImpl.java
@@ -0,0 +1,10 @@
+package com.yash.yits.dao.hibernate;
+
+import org.springframework.stereotype.Repository;
+
+import com.yash.yits.dao.IssueDao;
+
+@Repository
+public class IssueDaoImpl implements IssueDao {
+
+}
