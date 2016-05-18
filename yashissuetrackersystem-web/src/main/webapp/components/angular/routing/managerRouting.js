60c851c90981279979f5857955b90e40a00838c9
SomeshGautam91 <samkane007@gmail.com>
2016-05-18 14:39:23 +0530

    Common Error Handling Added.

--- yashissuetrackersystem-web/src/main/webapp/components/angular/routing/managerRouting.js
+++ yashissuetrackersystem-web/src/main/webapp/components/angular/routing/managerRouting.js
@@ -1,4 +1,33 @@
+function testInterceptor() {
+  return {
+	  		request: function(config) {
+	  			console.log("In Request Processing");
+	  			return config;
+	  		},
+	  		requestError: function(config){
+	  			console.error('Error while requesting for data');
+	  			return config;
+	  		},
+	  		response: function(res) {
+	  			console.log("In Response Processing");
+	  			return res;
+	  		},
+	  		responseError: function(res) {
+	  			console.error('Error while fetching members');
+				return $q.reject(res);
+	  		}
+  	}
+}
+
+
+
+
 var managerModule = angular.module('issueTrackingSystem.managerModule',['ngRoute','datatables']);
+
+managerModule.factory('testInterceptor', testInterceptor)
+managerModule.config(function($httpProvider) {
+  $httpProvider.interceptors.push('testInterceptor');
+})
 managerModule.config(function($routeProvider) {
 	
 	
--- yashissuetrackersystem-web/src/main/webapp/components/angular/routing/userRouting.js
+++ yashissuetrackersystem-web/src/main/webapp/components/angular/routing/userRouting.js
@@ -1,4 +1,33 @@
+function testInterceptor() {
+  return {
+	  		request: function(config) {
+	  			console.log("In Request Processing");
+	  			return config;
+	  		},
+	  		requestError: function(config){
+	  			console.error('Error while requesting for data');
+	  			return config;
+	  		},
+	  		response: function(res) {
+	  			console.log("In Response Processing");
+	  			return res;
+	  		},
+	  		responseError: function(res) {
+	  			console.error('Error while fetching members');
+				return $q.reject(res);
+	  		}
+  	}
+}
+
+
+
+
 var userModule = angular.module('issueTrackingSystem.userModule',['ngRoute','datatables']);
+
+userModule.factory('testInterceptor', testInterceptor)
+userModule.config(function($httpProvider) {
+  $httpProvider.interceptors.push('testInterceptor');
+})
 userModule.config(function($routeProvider){
 		$routeProvider
 			.when('/createIssue',{
@@ -7,14 +36,10 @@ userModule.config(function($routeProvider){
 			})
 
 			.when('/searchIssues', {
-		templateUrl : '../userIssues',
-		controller : 'userController as uc',
+				templateUrl : '../userIssues',
+				controller : 'userController as uc',
 		
-	})
-		
-
-			
-
+			})
 			.when('/editIssues',{
 				templateUrl:'../displayEditIssueForm',
 				controller:'userController as uc'
